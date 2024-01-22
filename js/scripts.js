57bbd3c79823a9451ff7eeaf8c1a6530eee5737d
Rosemary Ojwang <rosemaryojwang26@gmail.com>
2024-01-22 20:53:17 +0300

    debug

--- js/scripts.js
+++ js/scripts.js
@@ -9,30 +9,31 @@ function handleForm(event)  {
     let discountedPrice;
     let total;
 
-    if (purchase === Rompers)   {
+    if (purchase === 'Rompers')   {
+        console.log("kenya")
 
     } else if (purchase === 'DiaperVest' ) {
-
+        console.log("kenya")
     } else if (purchase === 'Socks')  {
-
+        console.log("kenya")
     }   else if (purchase === 'Shawl')  {
-
+        console.log("kenya")
     }   else if (purchase === 'Blanket')    {
-
+        console.log("kenya")
     }   else if (purchase === 'BabyCrib')   {
-
+        console.log("kenya")
     }   else if (purchase === 'Sweaters')   {
-
+        console.log("kenya")
     }   else if (purchase === 'Flannels')   {
-
+        console.log("kenya")
     }   else if (purchase === 'BabyBathBasin')   {
-
+        console.log("kenya")
     }   else if (purchase === 'ReceivingSet')   {
-
+        console.log("kenya")
     }   else if (purchase === 'Vests')   {
-
+        console.log("kenya")
     }   else if (purchase === 'babyWalker')   {
-
+        console.log("kenya")
     }
 }
 
