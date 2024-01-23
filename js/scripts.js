2dbabaa26f48eac298c3c168ee976048afc966da
Rosemary Ojwang <rosemaryojwang26@gmail.com>
2024-01-23 16:45:56 +0300

    display receipt and hide form

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,92 @@
 //Business Logic
 
+let discountedPrice;
+function rompersDiscount()  {
+    const rompersPrice = 100;
+    const percentageDiscount = 10;
+    discountedPrice = (100- percentageDiscount)/100 * rompersPrice;
+    return discountedPrice;
+
+}
+function diaperVestDiscount()   {
+    const percentageDiscount = 5;
+}
+function socksDiscount()    {
+    const percentageDiscount = 10;
+}
+function shawlDiscount()    {
+    const percentageDiscount = 25;
+}
+function blanketDiscount()  {
+    const percentageDiscount = 25;
+}
+function babyCribDiscount() {
+    const percentageDiscount = 15;
+}
+function sweatersDiscount() {
+    const percentageDiscount = 20;
+}
+function babyWalkerDiscount()   {
+    const percentageDiscount = 25;
+}
+function vestsDiscount()    {
+    const percentageDiscount = 5;
+}
+function receivingSetsDiscount()    {
+    const percentageDiscount = 15;
+}
+function babyBasinDiscount()    {
+    const percentageDiscount = 20;
+}
+function flannelsDiscount() {
+    const percentageDiscount = 5;
+}
 
 //UI Logic
 function handleForm(event)  {
     event.preventDefault();
     const purchase =document.getElementById("purchase").value;
     const quantity = document.getElementById("quantity").value;
-    let discountedPrice;
+    let receipt = document.getElementById("my-receipt");
+    receipt.style.display = "block"
+    let form = document.getElementById("items");
+    form.style.display = "none";
+    const dateNow = new Date();
+    document.getElementById("todays-date").innerHTML = dateNow;
+
     let total;
 
     if (purchase === 'Rompers')   {
-        console.log("kenya")
+        rompersDiscount();
+        total = discountedPrice * quantity;
 
     } else if (purchase === 'DiaperVest' ) {
-        console.log("kenya")
+        console.log("kenya1")
     } else if (purchase === 'Socks')  {
-        console.log("kenya")
+        console.log("kenya2")
     }   else if (purchase === 'Shawl')  {
-        console.log("kenya")
+        console.log("kenya3")
     }   else if (purchase === 'Blanket')    {
-        console.log("kenya")
+        console.log("kenya4")
     }   else if (purchase === 'BabyCrib')   {
-        console.log("kenya")
+        console.log("kenya5")
     }   else if (purchase === 'Sweaters')   {
-        console.log("kenya")
+        console.log("kenya6")
     }   else if (purchase === 'Flannels')   {
-        console.log("kenya")
+        console.log("kenya7")
     }   else if (purchase === 'BabyBathBasin')   {
-        console.log("kenya")
+        console.log("kenya8")
     }   else if (purchase === 'ReceivingSet')   {
-        console.log("kenya")
+        console.log("kenya9")
     }   else if (purchase === 'Vests')   {
-        console.log("kenya")
+        console.log("kenya10")
     }   else if (purchase === 'babyWalker')   {
-        console.log("kenya")
+        console.log("kenya11")
     }
 }
 
 window.addEventListener("load", function()  {
     const form = document.getElementById("items");
     form.addEventListener("submit", handleForm);
+    
 });
